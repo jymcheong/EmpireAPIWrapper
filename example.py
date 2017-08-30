55085d8a856667679558bf3bbae3292a93b1db88
jc <jym@jym.sg>
2017-08-30 14:21:41 +0800

    get rid of most of the pesky C0103 warnings.. !#@##$@D

--- example.py
+++ example.py
@@ -1,28 +1,29 @@
+"""This is a sample technique script using EmpireAPIWrapper"""
 import EmpireAPIWrapper
 
 
 if __name__ == "__main__":
                                     # change to your IP/host & (token or credentials)
-    api = EmpireAPIWrapper.empireAPI('empirec2', uname='empireadmin', passwd='Password123')
-    agent = api.agents()
-    agent_name = agent['agents'][0]['name']
-    
+    API = EmpireAPIWrapper.empireAPI('empirec2', uname='empireadmin', passwd='Password123')
+    AGENT = API.agents()
+    AGENT_NAME = AGENT['agents'][0]['name']
     try:
         # shell command example
-        data = {'Agent': agent_name, 'command': 'date'} # date is a rather slow command
-        taskID = api.agent_run_shell_cmd(agent_name, data)['taskID']
-        print('new taskID = ' + str(taskID))
-        print(api.agent_get_results(agent_name,taskID))
-        
+        DATA = {'Agent': AGENT_NAME, 'command': 'date'} # date is a rather slow command
+        TASKID = API.agent_run_shell_cmd(AGENT_NAME, DATA)['taskID']
+        print('new taskID = ' + str(TASKID))
+        print(API.agent_get_results(AGENT_NAME, TASKID))
+
         # module execution Job example
-        options = {'Agent': agent_name} # ie. gather from module's "info"
-        taskID = api.module_exec('powershell/situational_awareness/host/antivirusproduct', options)['taskID']
-        print('new taskID = ' + str(taskID))
-        print(api.agent_get_results(agent_name,taskID))
-       
+        OPTIONS = {'Agent': AGENT_NAME} # ie. gather from module's "info"
+        TASKID = API.module_exec('powershell/situational_awareness/host/antivirusproduct', \
+                                OPTIONS)['taskID']
+        print('new taskID = ' + str(TASKID))
+        print(API.agent_get_results(AGENT_NAME, TASKID))
+
         # module exception example (needs admin rights but session is non-elevated)
-        api.module_exec('powershell/situational_awareness/host/computerdetails', options)
-        api.agent_get
+        API.module_exec('powershell/situational_awareness/host/computerdetails', OPTIONS)
 
     except Exception as e:
         print('Error: ' + str(e))
+    
