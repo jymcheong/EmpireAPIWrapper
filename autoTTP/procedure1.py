46b6d482ffefce0aa56e00d92ae40000a71ce65a
jc <jym@jym.sg>
2017-08-30 16:33:34 +0800

    reordered autoTTP folder structure

--- autoTTP/procedure1.py
+++ autoTTP/procedure1.py
@@ -1,14 +1,14 @@
 """This module demonstrates procedure scripting"""
-import stage2.windows.deliverPayload.technique1
-import stage2.windows.deliverPayload.technique2
+import stage2.deliverPayload.windows.technique1
+import stage2.deliverPayload.windows.technique2
 
 try:
     # every technique script implements a run function
-    stage2.windows.deliverPayload.technique1.run("1.1.1.1")
-    print(stage2.windows.deliverPayload.technique1.run("2.2.2.2"))
+    stage2.deliverPayload.windows.technique1.run("1.1.1.1")
+    print(stage2.deliverPayload.windows.technique1.run("2.2.2.2"))
     
     # technique 2 is a folder module vs technique 1 which is a script
-    stage2.windows.deliverPayload.technique2.something('a','b')
+    stage2.deliverPayload.windows.technique2.something('a','b')
     
 except Exception as e:
     pass
