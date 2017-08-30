c09b7a701dd93eb4fc089a44421dab868c17f778
jc <jym@jym.sg>
2017-08-30 13:42:47 +0800

    added technique 2 vs 1 comments

--- autoTTP/procedure1.py
+++ autoTTP/procedure1.py
@@ -6,6 +6,8 @@ try:
     # every technique script implements a run function
     stage2.windows.deliverPayload.technique1.run("1.1.1.1")
     print(stage2.windows.deliverPayload.technique1.run("2.2.2.2"))
+    
+    # technique 2 is a folder module vs technique 1 which is a script
     stage2.windows.deliverPayload.technique2.something('a','b')
     
 except Exception as e:
